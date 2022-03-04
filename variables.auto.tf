4fa38e6e34f2d1a6657eb7da98c4427172ea65ad
jeonyoungwoo <26229501+jeonyoungwoo@users.noreply.github.com>
2022-03-04 18:07:36 +0900

    Update variables.auto.tf

--- variables.auto.tf
+++ variables.auto.tf
@@ -36,7 +36,7 @@ variable "organization" {
 variable "server_profile_action" {
   type        = string
   description = "Desired Action for the server profile (e.g., Deploy, Unassign)"
-  default     = "No-op"    //Deploy  ->> No-op  >> Deploy
+  default     = "Deploy"    //Deploy  ->> No-op  >> Deploy
 }
 
 variable "imc_access_policy" {
