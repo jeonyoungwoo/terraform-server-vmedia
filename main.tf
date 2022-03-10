ac21f12a4582fc6c64791d779ad0fce5a62e138f
jeonyoungwoo <26229501+jeonyoungwoo@users.noreply.github.com>
2022-03-11 03:54:35 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -234,7 +234,7 @@ resource "intersight_fabric_eth_network_group_policy" "network_group" {
   }
 }
 
-resource "intersight_vnic_nic0_if" "nic0" {
+resource "intersight_vnic_eth_if" "nic0" {
   # skip this resource if the name isn't defined
   count = var.lan_connectivity_policy == null ? 0 : 1
   name  = var.vnic_name
@@ -270,7 +270,7 @@ resource "intersight_vnic_nic0_if" "nic0" {
     moid = intersight_vnic_eth_qos_policy.ethernet_qos[count.index].id
   }
 }
-resource "intersight_fabric_0_network_group_policy" "network_group" {
+resource "intersight_fabric_eth_network_group_policy" "network_group" {
   # skip this resource if the name isn't defined
   count       = var.ethernet_network_group == null ? 0 : 1
   name        = var.ethernet_network_group
